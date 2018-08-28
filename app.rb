be46baf77867c5bae84dfffec095a7dfb66e410b
mguevara717 <marissa.guevara717@gmail.com>
2018-08-27 20:49:43 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		erb :index
 	end
+
+#inside get, updates our controller.
+
+		get "/info" do
+			erb :info
+		end
+
 end
