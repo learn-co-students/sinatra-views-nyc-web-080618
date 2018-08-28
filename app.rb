2bcad829ec86e383525c3f790237bc735fe90d8c
mguevara717 <marissa.guevara717@gmail.com>
2018-08-28 11:42:43 -0400

    Done.

--- app.rb
+++ app.rb
@@ -12,4 +12,16 @@ class App < Sinatra::Base
 			erb :info
 		end
 
+		get "/hello" do
+			erb :hello
+		end
+
+		get "/goodbye" do
+			erb :goodbye
+		end
+
+		get "/date" do
+			erb :date
+		end
+
 end
